9fe0a3591e449d77c256d8525e5e42bdddb52d4f
Anton Shchukin <a.a.shchukin@gmail.com>
2023-04-28 04:45:41 +0700

    Форматируем разметку

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,29 +1,30 @@
 import {createElement} from '../render.js';
 
 function createNewTaskButtonTemplate() {
-  return '' +
-    '<form class="trip-events__trip-sort  trip-sort" action="#" method="get">\n' +
-    '  <div class="trip-sort__item  trip-sort__item--day">\n' +
-    '    <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>\n' +
-    '    <label class="trip-sort__btn" for="sort-day">Day</label>\n' +
-    '  </div>\n' +
-    '  <div class="trip-sort__item  trip-sort__item--event">\n' +
-    '    <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>\n' +
-    '    <label class="trip-sort__btn" for="sort-event">Event</label>\n' +
-    '  </div>\n' +
-    '  <div class="trip-sort__item  trip-sort__item--time">\n' +
-    '    <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">\n' +
-    '    <label class="trip-sort__btn" for="sort-time">Time</label>\n' +
-    '  </div>\n' +
-    '  <div class="trip-sort__item  trip-sort__item--price">\n' +
-    '    <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">\n' +
-    '    <label class="trip-sort__btn" for="sort-price">Price</label>\n' +
-    '  </div>\n' +
-    '  <div class="trip-sort__item  trip-sort__item--offer">\n' +
-    '    <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>\n' +
-    '    <label class="trip-sort__btn" for="sort-offer">Offers</label>\n' +
-    '  </div>\n' +
-    '</form>';
+  return (`
+    <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+      <div class="trip-sort__item  trip-sort__item--day">
+        <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked>
+        <label class="trip-sort__btn" for="sort-day">Day</label>
+      </div>
+      <div class="trip-sort__item  trip-sort__item--event">
+        <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
+        <label class="trip-sort__btn" for="sort-event">Event</label>
+      </div>
+      <div class="trip-sort__item  trip-sort__item--time">
+        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
+        <label class="trip-sort__btn" for="sort-time">Time</label>
+      </div>
+      <div class="trip-sort__item  trip-sort__item--price">
+        <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price">
+        <label class="trip-sort__btn" for="sort-price">Price</label>
+      </div>
+      <div class="trip-sort__item  trip-sort__item--offer">
+        <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
+        <label class="trip-sort__btn" for="sort-offer">Offers</label>
+      </div>
+    </form>
+  `);
 }
 
 export default class Sort {
