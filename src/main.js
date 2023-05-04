0f060d471bddffc0a21cb5eb9ee6f6496b6d4c69
Anton Shchukin <a.a.shchukin@gmail.com>
2023-05-04 19:06:03 +0700

    Вьюшки для списка и точки маршрута

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import SortView from './view/sort-view.js';
+import ListView from './view/list-view.js';
 import {render} from './render.js';
+import ListViewPresenter from './presenters/list-view-presenter.js';
 
-const $tripEevents = document.querySelector('.trip-events');
+const tripEeventsElement = document.querySelector('.trip-events');
 
-render(new SortView(), $tripEevents);
+const listViewPresenter = new ListViewPresenter(tripEeventsElement);
+listViewPresenter.init();
