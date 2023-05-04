5a7cfa02868ea69111d34ac9fe092c5910a77351
Anton Shchukin <a.a.shchukin@gmail.com>
2023-05-04 19:19:40 +0700

    Вьюшки для создания и редактирования ивента

--- src/main.js
+++ src/main.js
@@ -1,6 +1,3 @@
-import SortView from './view/sort-view.js';
-import ListView from './view/list-view.js';
-import {render} from './render.js';
 import ListViewPresenter from './presenters/list-view-presenter.js';
 
 const tripEeventsElement = document.querySelector('.trip-events');
--- src/presenters/list-view-presenter.js
+++ src/presenters/list-view-presenter.js
@@ -1,6 +1,8 @@
 import SortView from'../view/sort-view.js';
 import ListView from'../view/list-view.js';
 import TripPointView from'../view/trip-point-view';
+import EventEditView from'../view/event-edit-view';
+import EventAddView from'../view/event-add-view';
 
 import {render} from '../render.js';
 
@@ -15,9 +17,12 @@ export default class ListViewPresenter {
   init() {
     render(new SortView(), this.container);
     render(this.listContainer, this.container);
+    render(new EventEditView(), this.listContainer.getElement());
+    render(new EventAddView(), this.listContainer.getElement());
 
     for( let i=0; i<3; i++ ) {
       render(new TripPointView(), this.listContainer.getElement());
     }
   }
+
 }
