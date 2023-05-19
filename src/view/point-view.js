d918a1995e5d2597a3295e76c8f4bc7284155cca
Anton Shchukin <a.a.shchukin@gmail.com>
2023-05-19 12:35:28 +0700

    offers

--- src/view/point-view.js
+++ src/view/point-view.js
@@ -15,6 +15,24 @@ function createPointTemplate({point, pointDestination, pointOffers}) {
     type,
   } = point;
 
+  const createOffers = (offers) => {
+
+    let offersTemplate = ``;
+
+    offers.forEach((offer) => {
+      offersTemplate += `
+        <li class="event__offer">
+          <span class="event__offer-title">${offer.title}</span>
+          +€&nbsp;
+          <span class="event__offer-price">${offer.price}</span>
+        </li>
+      `;
+    });
+
+    return offersTemplate;
+  }
+
+
   return (`
     <li class="trip-events__item">
       <div class="event">
@@ -36,13 +54,9 @@ function createPointTemplate({point, pointDestination, pointOffers}) {
         </p>
         <h4 class="visually-hidden">Offers:</h4>
         <ul class="event__selected-offers">
-          <li class="event__offer">
-            <span class="event__offer-title">${offers.service}</span>
-            +€&nbsp;
-            <span class="event__offer-price">${offers.price}</span>
-          </li>
+            ${createOffers(pointOffers)}
         </ul>
-        <button class="event__favorite-btn ${ isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
+        <button class="event__favorite-btn ${isFavorite ? 'event__favorite-btn--active' : ''}" type="button">
           <span class="visually-hidden">Add to favorite</span>
           <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
             <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
